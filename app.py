adefc25849e429d92b345d44661638132b0a13a4
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2024-09-08 18:33:09 -0300

    Atualizando app.py

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 from modelos.restaurante import Restaurante
-
+from modelos.cardapio.prato import Prato
+from modelos.cardapio.bebida import Bebida
 
 restaurante_boizao = Restaurante('bOizão', 'Currascaria')
 restaurante_forno = Restaurante('Forno de bArRo', 'Italiana')
 retaurante_beef = Restaurante('Baby-beef', 'mineira')
 
 restaurante_boizao.alternar_status()
-restaurante_boizao.receber_avaliacao('Daniel', 5)
-restaurante_boizao.receber_avaliacao('Daniel', 6)
-restaurante_boizao.receber_avaliacao('Isabella', 3)
-restaurante_boizao.receber_avaliacao('Polliana', 4)
+bebida_suco = Bebida('Suco de Laranja', 5.00, 'Grande')
+prato_paozinho = Prato('Paozinho', 2.00, 'O melho pão frances')
 
 def main():
-  Restaurante.listar_restaurantes()
+  print(bebida_suco)
+  print(prato_paozinho)
 
 if __name__ == '__main__':
   main()
