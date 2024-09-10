4a8cd8ae7b8b9d9fd068144edb8447be10f27788
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2024-09-09 22:13:17 -0300

    Adicionando metodos adiciona e exibe cardapio

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,5 +1,5 @@
 from modelos.avaliacao import Avaliacao
-import os
+from modelos.cardapio.item_cardapio import ItemCardapio
 
 class Restaurante:
   '''Cria um novo restaurante'''
@@ -17,6 +17,7 @@ class Restaurante:
     self._categoria = categoria.upper()
     self._ativo = False
     self._avaliacao = []
+    self._cardapio = []
     Restaurante.restaurantes.append(self)
 
   def __str__(self):
@@ -64,6 +65,21 @@ class Restaurante:
     quantidade_de_notas = len(self._avaliacao)
     media = round(soma_das_notas / quantidade_de_notas, 1)
     return media
+
+  def adiciona_no_cardapio(self, item):
+    if isinstance(item, ItemCardapio):
+      self._cardapio.append(item)
+  
+  @property
+  def exibir_cardapio(self):
+    print(f'Cardapio do restaurante {self._nome}\n')
+    for i, item in enumerate(self._cardapio, start=1):
+      if hasattr(item, 'descricao'):
+        mensagem_prato = f'{i}. Nome: {item._nome} | R${item._preco} | Descricao: {item.descricao}'
+        print(mensagem_prato)
+      else:
+        mensagem_bebida = f'{i}. Nome: {item._nome} | R${item._preco} | Tamanho: {item.tamanho}'
+        print(mensagem_bebida)
 
 
 
